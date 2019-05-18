862f3d935319a401df5b662bb5938f9c0bfe78e8
brianlopezm <lopezmbrian@gmail.com>
2019-05-18 12:15:36 -0300

    Se agrega getJornada

--- api/routes/partidos.js
+++ api/routes/partidos.js
@@ -5,6 +5,7 @@ var api = express.Router();
 
 api.get('/partidos/getGames/:seleccion',partidosController.getGamesByTeam);
 api.get('/partidos/getGames/',partidosController.getAllGames);
+api.get('/partidos/getJornada/:jornada',partidosController.getGamesByJornada);
 api.patch('/partidos/updateGame/:id', partidosController.updateGame);
 api.post('/partidos/saveGame',partidosController.savePartido);
 api.delete('/partidos/deleteGame/:id',partidosController.deletePartido);
